99745b9e7663bd3e49c3ef5b0a3945c768b124be
faridalam61 <faridalam62@gmail.com>
2023-05-16 18:43:28 +0600

    Update user added

--- src/Update.jsx
+++ src/Update.jsx
@@ -9,6 +9,16 @@ function Update() {
     const form = e.target;
     const name = form.name.value;
     const email = form.email.value;
+    const updatedUser = {name, email}
+    fetch(`http://localhost:5000/users/${user._id}`,{
+      method: 'PUT',
+      headers:{
+        'content-type' : 'application/json'
+      },
+      body:JSON.stringify(updatedUser)
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
   };
   return (
     <div className="card w-96 mx-auto bg-base-100 shadow-xl">
